9955b5656832212244ae92795b454dc7dee844e1
Nina E Schulz <ninaschulz_16@hotmail.com>
2025-09-28 23:21:36 +0100

    This in not a test afterall, it is my project

--- client/src/main.js
+++ client/src/main.js
@@ -1,50 +1,35 @@
-const jokeDisplaySection = document.getElementById('app')
+const guestbookSection = document.getElementById('app')
 
-//Get all the jokes from our express API and display
 
-// how do we fetch data??
-//fetch () function
-//create a function to fetch data
 
-async function fetchJokes() {
-    //fetch() by default send a GET request 
-    const response = await fetch ('https://testing-githubpush.onrender.com/jokes')
-    const jokes = await response.json()
-    createJokes(jokes)
+async function fetchEntries() {
+    
+    const response = await fetch ('https://testing-githubpush.onrender.com/entries')
+    const entries = await response.json()
+    displayEntries(entries);
 }
-function createJokes (jokesArray) {
-    jokesArray.forEach((singleJoke)=>{
+function displayEntries(entriesArray) {
+   
+    guestbookSection.innerHTML = '';
+    
+    entriesArray.forEach((entry)=>{
+       
         const div = document.createElement('div')
 
-        console.log(singleJoke)
+        console.log(entry)
         
-        const jokeElement = document.createElement('p')
-        const punchlineElement = document.createElement('p')
-    //set the p element we made to have text of the joke
-    jokeElement.innerText =singleJoke.joke;
-    punchlineElement.innerText = singleJoke.punchline;
+        const messengerElement = document.createElement('p')
+        const messageElement = document.createElement('p')
+    
+    messengerElement.innerText = entry.messenger +  "wrote";
+    messageElement.innerText = entry.message;
 
-        div.setAttribute('class','joke-container')
-        div.append(jokeElement, punchlineElement)
-        jokeDisplaySection.append(div)
+        div.setAttribute('class','entry-container')
+        div.append(messengerElement, messageElement)
+        guestbookSection.append(div)
 
-    })
+    });
 }
-
-/*
-{
-"id": 1,
-"joke": "Why did the scarecrow win an award?",
-"punchline": "Because he was outstanding in his field."
-}
-<div>
-    <p>joke</p>
-</div>
-*/
-
-fetchJokes()
-
-//Lets work on a form so users can create new jokes
 
 const form = document.getElementById('form')
 
@@ -52,15 +37,17 @@ form.addEventListener('submit', async (event)=>{
     event.preventDefault()
 
     const data = new FormData(form)
-    const userJoke = Object.fromEntries(data)
-    console.log(userJoke)
-
-    //Now I need to send a POST request to my server
-    const response = await fetch('https://testing-githubpush.onrender.com/jokes',{
+    const userEntry = Object.fromEntries(data)
+    console.log(userEntry)
+   
+    const response = await fetch('https://testing-githubpush.onrender.com/entries',{
         headers:{
                 "Content-Type": "application/json"
         },
         method: "POST",
-        body: JSON.stringify(userJoke)
+        body: JSON.stringify(userEntry)
     })
+
+    form.reset();
+    fetchEntries();
 })
--- server/server.js
+++ server/server.js
@@ -3,21 +3,18 @@ import dotenv from "dotenv"
 import cors from "cors"
 import pg from "pg"
 
-//instansiate the server 
+
 
 const app = express ()
 
-//setup middleware (annoying config things)
-
-//so our server can read json
 
 app.use(express.json())
-//loads our environment variable
+
 dotenv.config()
-//allows our server to talk to other servers
+
 app.use(cors())
 
-// set up database connection
+
 
 const db = new pg.Pool({
     connectionString: process.env.DB_CONN
@@ -25,29 +22,27 @@ const db = new pg.Pool({
 app.get('/', (req, res)=>{
     res.status(200).json('You are looking at my root route! How dare you!')
 })
- //quering our database is asynchronous.
+ 
 
- app.get('/jokes', async (req,res)=>{
-    //fetch all jokes from sql table
+ app.get('/entries', async (req,res)=>{
+    
+    const result =await db.query('SELECT * FROM guestbook ORDER BY created_at DESC');
+    res.json(result.rows);
+ });
 
-    const result =await db.query('SELECT * FROM jokes')
-    res.json(result.rows)
- })
 
- //I'll make a post route to allow people to make new jokes
-
- app.post ('/jokes', async(req,res)=>{
-    //When the client sends up information it is always in the request.body
+ app.post ('/entries', async(req,res)=>{
+    
     const body = req.body
     
-    const jokeFromClient = req.body.joke
-    const punchlineFromClient = req.body.punchline 
+    const messengerFromClient = req.body.messenger
+    const messageFromClient = req.body.message
 
-    //we use $1, $2 as a placeholders so we aren't just putting whatever sends us in the string.
 
-    const data = await db.query('INSERT INTO jokes(joke, punchline) VALUES ($1,$2)', [jokeFromClient, punchlineFromClient])
+    const data = await db.query('INSERT INTO guestbook(messenger, message) VALUES ($1,$2)', 
+        [messengerFromClient, messageFromClient])
 
-    res.json ({status: 'Joke inserted into database'})
+    res.json ({status: 'New message submitted in guestbook'})
  })
 
 
